fc6237188edf8f8a53a77ab930a04efa3b40d566
Gabriel Nakhata <gabrielnakata@gmail.com>
2022-01-23 00:42:03 -0300

    first commit...

---
+++ app-cadastro-series-em-memoria/app-cadastro-series-em-memoria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app_cadastro_series_em_memoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
